3c715724502e50cb106eff59689dba47da14461d
IvanAnceno <mekcpsu2@gmail.com>
2024-12-26 12:12:32 +0800

    Update Router.jsx

--- myreact/src/router/Router.jsx
+++ myreact/src/router/Router.jsx
@@ -1,20 +1,19 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route } from "react-router-dom"; // Note: should be 'react-router-dom', not 'react-router'
 import HomePage from "../pages/HomePage";
 import ProductPage from "../pages/ProductPage";
 import Delivery from "../pages/Delivery";
 import Contact from "../pages/Contact";
 import MainLayout from "../layout/MainLayout";
 
-
 function Router() {
-
     return (
         <Routes>
-            <Route element={<MainLayout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/products" element={<ProductPage />} />
-            <Route path="/delivery" element={<Delivery />} />
-            <Route path="/contact" element={<Contact />} />
+            {/* MainLayout will be the wrapper for all the routes below it */}
+            <Route path="/" element={<MainLayout />}>
+                <Route index element={<HomePage />} /> {/* Default Route */}
+                <Route path="products" element={<ProductPage />} />
+                <Route path="delivery" element={<Delivery />} />
+                <Route path="contact" element={<Contact />} />
             </Route>
         </Routes>
     );
